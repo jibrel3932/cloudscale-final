4a501fb2d5eaddb5a1e6d34befba79635d30f353
Jibrel Abubakr Jibrel <your-github-email@example.com>
2026-06-26 01:57:46 +0200

    Add Terraform outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "kube_config_command" {
+  value = "az aks get-credentials --resource-group ${azurerm_resource_group.rg.name} --name ${azurerm_kubernetes_cluster.aks.name}"
+}
